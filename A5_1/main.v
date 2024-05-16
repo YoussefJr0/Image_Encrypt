ee2d3b46e93fc576190e15f44b475eaeb0559aa8
YoussefJr0 <youssefmahmou123@gmail.com>
2024-05-16 23:01:50 +0300

    initial commit

---
+++ A5_1/main.v
@@ -0,0 +1,32 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    22:22:40 05/16/2024 
+// Design Name: 
+// Module Name:    main 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module main(
+	input wire clk,
+	input wire reset,
+	input wire [63:0] secret_key,
+	input wire [21:0] public_key,
+
+    );
+
+	
+
+
+endmodule
